729cc979153c9a35884889178e554c72d7da22b9
razzamatazm <tyler.hereford@gmail.com>
2026-02-06 13:58:58 -0800

    Enable navigation blocking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { MantineProvider, createTheme } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import App from './App'
 
 import '@mantine/core/styles.css'
@@ -23,14 +24,16 @@ const queryClient = new QueryClient({
   },
 })
 
+const history = createBrowserHistory()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <MantineProvider theme={theme} defaultColorScheme="dark">
         <Notifications position="top-right" />
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <HistoryRouter history={history}>
           <App />
-        </BrowserRouter>
+        </HistoryRouter>
       </MantineProvider>
     </QueryClientProvider>
   </React.StrictMode>
